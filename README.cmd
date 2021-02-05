765f047d54ed3d2390c54996b22edded0bee7627
vincent19974 <45345593+vincent19974@users.noreply.github.com>
2021-02-05 09:00:17 +0800

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,1 +1,2 @@
 run first the update-database.py to set all getconfig=0 and  to update the database as well
+Note that this scripts are running in a wide variety of network vpn (Openvpn) nationwide for approximately 4000 routers.
